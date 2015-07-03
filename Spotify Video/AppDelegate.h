1e0141fde256f08dc84540066828d867808254db
yene <yene@cocoadojo.com>
2015-07-03 17:04:20 +0200

    adding all files

---
+++ Spotify Video/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spotify Video
+//
+//  Created by Yannick Weiss on 03/07/15.
+//  Copyright © 2015 Yannick Weiss. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Spotify Video/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Spotify Video
+//
+//  Created by Yannick Weiss on 03/07/15.
+//  Copyright © 2015 Yannick Weiss. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
