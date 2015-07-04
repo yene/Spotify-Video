f63789a9f1beee865114d667e974c35b69b2fb35
yene <yene@cocoadojo.com>
2015-07-04 21:22:44 +0200

    playing youtube video

--- Spotify Video/AppDelegate.m
+++ Spotify Video/AppDelegate.m
@@ -9,9 +9,13 @@
 #import "AppDelegate.h"
 #import <ScriptingBridge/SBApplication.h>
 #import "Spotify.h"
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVPlayer.h>
+#import <XCDYouTubeKit/XCDYouTubeKit.h>
+
 
 @interface AppDelegate ()
-
+@property (weak) IBOutlet AVPlayerView *playerView;
 @property (weak) IBOutlet NSWindow *window;
 @end
 
@@ -35,21 +39,26 @@
   }
   
   SpotifyApplication *spotify = [SBApplication applicationWithBundleIdentifier:@"com.spotify.client"];
-  double bla = spotify.playerPosition;
-  if (spotify.playerState == SpotifyEPlSPlaying) {
-    SpotifyTrack *track = spotify.currentTrack;
-    NSString *songName = track.name;
+  if (spotify.playerState == SpotifyEPlSStopped) {
+    NSLog(@"Spotify is stopped");
+    return;
   }
   
-  NSURL *url = [NSURL URLWithString:@"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=viewCount&q=rick+roll&type=video&key=AIzaSyD9sGERE6yX4KvsGGOExAyaAitv7ODOHAY"];
-  NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+  SpotifyTrack *track = spotify.currentTrack;
+  NSString *apiURL = @"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=viewCount&type=video&key=AIzaSyD9sGERE6yX4KvsGGOExAyaAitv7ODOHAY&q=";
+  NSString *searchString = [NSString stringWithFormat:@"%@ %@", track.name, track.artist];
+  searchString = [searchString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
+  NSString *url = [apiURL stringByAppendingString:searchString];
+  NSURL *urlurl = [NSURL URLWithString:url];
+  NSURLRequest *urlRequest = [NSURLRequest requestWithURL:urlurl];
   NSURLResponse* response = nil;
-  NSData* data = [NSURLConnection sendSynchronousRequest:urlRequest returningResponse:&response error:nil];
+  NSError *error = nil;
+  NSData* data = [NSURLConnection sendSynchronousRequest:urlRequest returningResponse:&response error:&error];
   
   
   
   // TODO: more checking like http://stackoverflow.com/a/7794561/279890
-  NSError *error = nil;
+  
   id object = [NSJSONSerialization
                JSONObjectWithData:data
                options:0
@@ -57,8 +66,28 @@
 
   NSDictionary *results = object;
   NSArray *items = [results objectForKey:@"items"];
-  NSString *videoID = [items[0] valueForKeyPath:@"id.videoId"];
+  NSString *videoIdentifier = [items[0] valueForKeyPath:@"id.videoId"];
   // https://www.youtube.com/watch?v=dQw4w9WgXcQ
+  
+  [[XCDYouTubeClient defaultClient] getVideoWithIdentifier:videoIdentifier completionHandler:^(XCDYouTubeVideo *video, NSError *error) {
+    if (video)
+    {
+      // Do something with the `video` object
+      NSDictionary *streamURLs = video.streamURLs;
+      NSURL *url = streamURLs[XCDYouTubeVideoQualityHTTPLiveStreaming] ?: streamURLs[@(XCDYouTubeVideoQualityHD720)] ?: streamURLs[@(XCDYouTubeVideoQualityMedium360)] ?: streamURLs[@(XCDYouTubeVideoQualitySmall240)];
+      AVPlayer *player = [AVPlayer playerWithURL:url];
+      player.volume = 0;
+      self.playerView.player = player;
+      
+      [player play];
+    }
+    else
+    {
+      // Handle error
+      [[NSAlert alertWithError:error] runModal];
+    }
+  }];
+  
 }
 
 @end
